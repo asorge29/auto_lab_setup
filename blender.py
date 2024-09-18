95f63d61ade76d4edb3413efb347be3e12805973
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-09-17 21:56:25 -0700

    todos and experimenting with cross-platform compatibility

--- blender.py
+++ blender.py
@@ -2,31 +2,45 @@ import subprocess
 from os import path
 import zipfile
 from platform import system
+#FIXME
+def get_windows_desktop():
+    if path.exists(path.expanduser('~\\OneDrive\\Desktop')):
+        return path.expanduser('~\\OneDrive\\Desktop')
+    else:
+        return path.expanduser('~\\Desktop')
 
 def install_blender(zip_url:str = 'https://mirrors.iu13.net/blender/release/Blender4.2/blender-4.2.1-windows-x64.zip'):
     system_name = system()
     print(f'Detected system: {system_name}')
-    desktop_path = path.expanduser('~/Desktop')
+#TODO make platform specific adjustments
+    if system_name == 'Windows':
+        desktop_path = get_windows_desktop()
+        logical_and = ';'
+    else:
+        desktop_path = path.expanduser('~\\Desktop')
+        logical_and = '&&'
     print(f'Desktop path: {desktop_path}')
-    install_command = f'cd {desktop_path} && curl {zip_url} --output blender.zip'
+#FIXME windows ps script with iwr and OutFile
+    install_command = f'cd {desktop_path} {logical_and} curl {zip_url} --output blender.zip'
     print(f'Running: {install_command}')
     print('Downloading Blender...')
     subprocess.run([install_command], shell=True, text=True)
     zip_path = path.join(desktop_path, 'blender.zip')
     print(f'Blender zip path: {zip_path}')
+
     print('Extracting Blender...')
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(desktop_path)
     if system_name == 'Windows':
-        delete_command = f'cd {path.jdesktop_path} && del blender.zip'
+        delete_command = f'cd {path.expanduser('~')}; del blender.zip'
         subprocess.run([delete_command])
         print('Cleaning up...')
     elif system_name == 'Linux':
-        delete_command = f'cd {desktop_path} && rm {zip_path}'
+        delete_command = f'cd {path.expanduser('~')} && rm {zip_path}'
         subprocess.run([delete_command])
         print('Cleaning up...')
     elif system_name == 'Darwin':
-        delete_command = f'cd {desktop_path} && rm {zip_path}'
+        delete_command = f'cd {path.expanduser('~')} && rm {zip_path}'
         subprocess.run([delete_command])
         print('Cleaning up...')
     else:
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from git_setup import setup_git
 
 setup_git()
+
+#TODO make outputs pretty and function like cli with args
+#TODO Unity Hub, Blender, Git, VSCode
