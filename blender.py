128c2511b1688db936abbab8aaa9111f8e839173
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-09-19 09:42:57 -0700

    updated command structure

--- blender.py
+++ blender.py
@@ -19,10 +19,10 @@ def install_blender(zip_url:str = 'https://mirrors.iu13.net/blender/release/Blen
     print(f'Desktop path: {desktop_path}')
     print(f'Downloading Blender from {zip_url}')
 
-    install_command = f'cd {desktop_path}; iwr {zip_url} --OutFile blender.zip' if system_name == 'Windows' else f'cd {desktop_path}; curl {zip_url} --output blender.zip'
+    install_command = f'cd {desktop_path}; iwr {zip_url} -OutFile blender.zip' if system_name == 'Windows' else f'cd {desktop_path}; curl {zip_url} --output blender.zip'
     print(f'Running: {install_command}')
     print('Downloading Blender...')
-    subprocess.run([install_command], shell=True, text=True)
+    subprocess.run([f'cd {desktop_path}', f'iwr '], shell=True, text=True)
     print('Download complete!')
 
     zip_path = path.join(desktop_path, 'blender.zip')
@@ -49,5 +49,61 @@ def install_blender(zip_url:str = 'https://mirrors.iu13.net/blender/release/Blen
 
     print('Blender successfully installed!')
     
+if __name__ == '__main__':
+    install_blender()
+
+
+import subprocess
+from os import path
+import zipfile
+from platform import system
+
+def get_windows_desktop():
+    if path.exists(path.expanduser('~/OneDrive/Desktop')):
+        return path.expanduser('~/OneDrive/Desktop')
+    else:
+        return path.expanduser('~/Desktop')
+
+def install_blender(zip_url: str = 'https://mirrors.iu13.net/blender/release/Blender4.2/blender-4.2.1-windows-x64.zip'):
+    system_name = system()
+    print(f'Detected system: {system_name}')
+    
+    if system_name == 'Windows':
+        desktop_path = get_windows_desktop()
+    else:
+        desktop_path = path.expanduser('~/Desktop')
+    
+    print(f'Desktop path: {desktop_path}')
+    print(f'Downloading Blender from {zip_url}')
+    
+    zip_path = path.join(desktop_path, 'blender.zip')
+    
+    if system_name == 'Windows':
+        # Prepare the download command
+        install_command = ['powershell', '-Command', f'Invoke-WebRequest {zip_url} -OutFile "{zip_path}"']
+    else:
+        install_command = ['curl', zip_url, '--output', zip_path]
+    
+    print(f'Running: {" ".join(install_command)}')
+    print('Downloading Blender...')
+    
+    # Use subprocess.run correctly
+    subprocess.run(install_command, check=True)
+    print('Download complete!')
+
+    print('Extracting Blender...')
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(desktop_path)
+
+    print('Cleaning up...')
+    delete_command = None
+    if system_name == 'Windows':
+        delete_command = ['cmd', '/C', f'del "{zip_path}"']
+    else:
+        delete_command = ['rm', zip_path]
+
+    subprocess.run(delete_command, check=True)
+    print('Blender successfully installed!')
+
 if __name__ == '__main__':
     install_blender()
